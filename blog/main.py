03c88f2840a5e8daf375866fd7e96e2e80fc4162
vaibhavgupta082 <86800548+vaibhavgupta082@users.noreply.github.com>
2025-11-10 22:58:45 +0530

    adding Crud for user entity

--- blog/main.py
+++ blog/main.py
@@ -52,3 +52,44 @@ def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(blog.first())
     return "updated"
+
+#create same CRUD for User too
+
+@app.post("/user")
+def create_user(request:schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get("/user/{id}", response_model=schemas.User)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
+    return user 
+
+@app.get("/users", response_model=List[schemas.User])
+def all_users(db: Session = Depends(get_db)):
+    users = db.query(models.User).all()
+    return users
+
+@app.delete("/user/{id}")
+def delete_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
+    user.delete(synchronize_session=False)
+    db.commit()
+    return "done"
+
+@app.put("/user/{id}")
+def update_user(id: int, request: schemas.User, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+    if not user.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
+    user.update({'name': request.name, 'email': request.email, 'password': request.password})
+    db.commit()
+    db.refresh(user.first())
+    return "updated"
