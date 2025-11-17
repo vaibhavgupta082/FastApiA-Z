6eac60892f326275c86f946ae4ea7520758a6693
vaibhavgupta082 <86800548+vaibhavgupta082@users.noreply.github.com>
2025-11-17 10:52:25 +0530

    adding repository folder

--- blog/routers/Blog.py
+++ blog/routers/Blog.py
@@ -4,6 +4,7 @@ from ..database import get_db
 from ..schemas import ShowBlog, Blog, BlogBase
 from .. import models
 from sqlalchemy.orm import Session
+from .. repository.Blog import get_all, create, get_by_id, delete , update
 
 
 router = APIRouter(tags=["Blogs"], prefix="/blog")
@@ -11,52 +12,25 @@ router = APIRouter(tags=["Blogs"], prefix="/blog")
 
 @router.get("/blog", response_model=List[ShowBlog])
 def all_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return get_all(db)
 
 
 @router.get("/{id}", response_model=ShowBlog)
 def blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with the id {id} is not available",
-        )
-    return blog
+    return get_by_id(db, id)
+
 
 
 @router.post("/", response_model=BlogBase)
 def create_blog(request: Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return create(request, db)
 
 
 @router.delete("/{id}")
 def delete_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with the id {id} is not available",
-        )
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "done"
+    return delete(db, id)
 
 
 @router.put("/{id}")
 def update_blog(id: int, request: Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with the id {id} is not available",
-        )
-    blog.update({"title": request.title, "body": request.body})
-    db.commit()
-    db.refresh(blog.first())
-    return "updated"
+    return update(db, id, request)
--- blog/routers/User.py
+++ blog/routers/User.py
@@ -4,64 +4,31 @@ from .. import models, schemas
 from typing import List
 from ..database import get_db
 from .. import hashing
+from ..repository.User import get_all, create, get_by_id , delete, update
 
 router = APIRouter(tags=["Users"], prefix="/user")
 
 
 @router.post("/", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name,
-        email=request.email,
-        password=hashing.Hash.bcrypt(request.password),
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return create(request, db)
 
 
 @router.get("/{id}", response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with the id {id} is not available",
-        )
-    return user
+    return get_by_id(db, id)
 
 
 @router.get("/", response_model=List[schemas.ShowUser])
 def all_users(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
+    return get_all(db)
 
 
 @router.delete("/{id}")
 def delete_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id)
-    if not user.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with the id {id} is not available",
-        )
-    user.delete(synchronize_session=False)
-    db.commit()
-    return "done"
+    return delete(db, id)
 
 
 @router.put("/{id}")
 def update_user(id: int, request: schemas.User, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id)
-    if not user.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with the id {id} is not available",
-        )
-    user.update(
-        {"name": request.name, "email": request.email, "password": request.password}
-    )
-    db.commit()
-    db.refresh(user.first())
-    return "updated"
+    return update(db, id, request)
